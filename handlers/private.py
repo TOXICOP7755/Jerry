730a2377d1422db06b49c97dcd590ada7e02de17
TeamUzox <106080156+TeamUzox@users.noreply.github.com>
2022-07-17 18:46:40 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -7,7 +7,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
 
-@Client.on_message(command("start") & filters.private & ~filters.edited)
+@Client.on_message(filters.command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/19a3795338160236661b0.jpg",
